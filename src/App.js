a74755ff5599c2f574256f0b71ce6b6a0344593d
Georleshia Minter <georleshiam@gmail.com>
2023-06-06 11:08:52 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+import axios from "axios";
+import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -18,6 +20,11 @@ function App() {
             <Route 
               exact path="/"
               element={<BestBooks />}
+            >
+            </Route>
+            <Route 
+              exact path="/About"
+              element={<About />}
             >
             </Route>
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
+import Carousel from 'react-bootstrap/Carousel';
+
+
 
 function BestBooks(){
-
+  const [books, setBooks] = useState([])
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  let booksDataHTML = books.map((element)=>{ return       <Carousel.Item>
+    {element.title}</Carousel.Item>
+})
 
+  axios.get('https://can-of-books-ppja.onrender.com/books')
+   .then(function (response) {
+    setBooks(response.data)
 
+     // handle success
+   console.log(response.data);
+  })
 
     /* TODO: render all the books in a Carousel */
-
+    if (booksDataHTML.length === 0) {
+      booksDataHTML = <p>"No books found."</p>;
+    } else {
+      console.log("Found " + booksDataHTML.length + " books.");
+      // do something with the booksDataHTML variable
+    }
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        {booksDataHTML}
+        {books.length ? (
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+<Carousel>
+    {booksDataHTML}
+</Carousel>
+
+          
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>No Books Found :</h3>
         )}
       </>
     )
